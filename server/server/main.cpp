70597292257fc6c187cc1b77dff09a5781e60f6a
nemanjarotman <34609928+nemanjarotman@users.noreply.github.com>
2018-03-30 01:44:30 +0200

    Added server, some problem with fopen(vs 2015)

---
+++ server/server/main.cpp
@@ -0,0 +1,133 @@
+#undef UNICODE
+
+#define WIN32_LEAN_AND_MEAN
+
+#include <windows.h>
+#include <winsock2.h>
+#include <ws2tcpip.h>
+#include <stdlib.h>
+#include <stdio.h>
+
+#pragma comment (lib, "Ws2_32.lib")
+#pragma comment (lib, "Mswsock.lib")
+
+#define BUFFER 512
+#define PORT "27015"
+
+int __cdecl main(void)
+{
+	WSADATA wsaData;
+	int iResult;
+
+	SOCKET ListenSocket = INVALID_SOCKET;
+	SOCKET ClientSocket = INVALID_SOCKET;
+
+	struct addrinfo *result = NULL;
+	struct addrinfo x;
+
+	int iSendResult;
+	char receive_buf[BUFFER];
+	int recvbuflen = BUFFER;
+
+	iResult = WSAStartup(MAKEWORD(2, 2), &wsaData);
+	if (iResult != 0) {
+		printf("WSAStartup failed with error: %d\n", iResult);
+		return 1;
+	}
+
+	ZeroMemory(&x, sizeof(x));
+	x.ai_family = AF_INET;
+	x.ai_socktype = SOCK_STREAM;
+	x.ai_protocol = IPPROTO_TCP;
+	x.ai_flags = AI_PASSIVE;
+
+	iResult = getaddrinfo(NULL, PORT, &x, &result);
+	if (iResult != 0) {
+		printf("getaddrinfo failed with error: %d\n", iResult);
+		WSACleanup();
+		return 1;
+	}
+
+	ListenSocket = socket(result->ai_family, result->ai_socktype, result->ai_protocol);
+	if (ListenSocket == INVALID_SOCKET) {
+		printf("socket failed with error: %ld\n", WSAGetLastError());
+		freeaddrinfo(result);
+		WSACleanup();
+		return 1;
+	}
+
+	iResult = bind(ListenSocket, result->ai_addr, (int)result->ai_addrlen);
+	if (iResult == SOCKET_ERROR) {
+		printf("bind failed with error: %d\n", WSAGetLastError());
+		freeaddrinfo(result);
+		closesocket(ListenSocket);
+		WSACleanup();
+		return 1;
+	}
+
+	freeaddrinfo(result);
+
+	iResult = listen(ListenSocket, SOMAXCONN);
+	if (iResult == SOCKET_ERROR) {
+		printf("listen failed with error: %d\n", WSAGetLastError());
+		closesocket(ListenSocket);
+		WSACleanup();
+		return 1;
+	}
+
+	ClientSocket = accept(ListenSocket, NULL, NULL);
+	if (ClientSocket == INVALID_SOCKET) {
+		printf("accept failed with error: %d\n", WSAGetLastError());
+		closesocket(ListenSocket);
+		WSACleanup();
+		return 1;
+	}
+
+
+	printf("Client socket accepted!\n");
+	printf("Client connected!\n");
+	printf("Server is ready for receiving...\n");
+
+	closesocket(ListenSocket);
+
+	FILE *file = NULL;
+	size_t bytesRead = 0;
+
+	while ((iResult = recv(ClientSocket, receive_buf, recvbuflen, 0))>0) {
+
+		fwrite(receive_buf, sizeof(char), recvbuflen, file);
+		bytesRead += recvbuflen;
+
+	}
+	fclose(file);
+
+	printf("bytes received %d\n", bytesRead);
+	printf("File is received and saved on desktop :)\n");
+
+
+	do {
+
+		if (iResult == 0)
+			printf("Connection closing...\n");
+		else {
+			printf("recv failed with error: %d\n", WSAGetLastError());
+			closesocket(ClientSocket);
+			WSACleanup();
+			return 1;
+		}
+
+	} while (iResult > 0);
+
+	iResult = shutdown(ClientSocket, SD_SEND);
+	if (iResult == SOCKET_ERROR) {
+		printf("shutdown failed with error: %d\n", WSAGetLastError());
+		closesocket(ClientSocket);
+		WSACleanup();
+		return 1;
+	}
+
+	closesocket(ClientSocket);
+	WSACleanup();
+
+	return 0;
+}
